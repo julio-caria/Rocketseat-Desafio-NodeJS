6da06490f00136a91411d2053244cf1fe98f548b
Julio Caria <julio.caria@hotmail.com>
2025-08-16 02:18:06 -0300

    chore: change project structure

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+import fastify from "fastify";
+import { fastifySwagger } from "@fastify/swagger";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { getCourseByIdRoute } from "./routes/get-course-by-id.ts";
+import { getCoursesRoute } from "./routes/get-courses.ts";
+import { createCourseRoute } from "./routes/create-course.ts";
+import scalarAPIReference from "@scalar/fastify-api-reference";
+
+const server = fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+}).withTypeProvider<ZodTypeProvider>();
+
+if (process.env.NODE_ENV === "development") {
+  server.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: "Rocketseat - Desafio Node.js",
+        version: "1.0.0",
+      },
+    },
+    transform: jsonSchemaTransform,
+  });
+
+  server.register(scalarAPIReference, {
+    routePrefix: "/docs",
+    configuration: {
+      title: "Docs - Challenge Node.js API Reference",
+      theme: "deepSpace",
+    },
+  });
+}
+
+server.setSerializerCompiler(serializerCompiler);
+server.setValidatorCompiler(validatorCompiler);
+
+server.register(getCoursesRoute);
+server.register(getCourseByIdRoute);
+server.register(createCourseRoute);
+
+export { server }
